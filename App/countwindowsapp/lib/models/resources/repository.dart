32262c89c5de0c2dd6cbeee9770b918e99c0288d
Anton Forsvall <antonforsvall@gmail.com>
2019-10-24 19:20:25 +0200

    added bloc and shared preferences for the login site

---
+++ App/countwindowsapp/lib/models/resources/repository.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+import 'api.dart';
+import 'package:countwindowsapp/models/users/user.dart';
+
+class Repository {
+  final apiProvider = ApiProvider();
+
+  Future<User> registerUser(String firstname, String lastname, String username, String email, String password) =>
+  apiProvider.registerUser(firstname, lastname, username, email, password);
+
+  Future<User> signinUser(String username,String password, String apiKey) =>
+  apiProvider.signinUser(username, password, apiKey);
+}
---
+++ App/countwindowsapp/lib/models/users/user_bloc_provider.dart
@@ -0,0 +1,28 @@
+import 'package:countwindowsapp/models/resources/repository.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:countwindowsapp/models/users/user.dart';
+
+class UserBloc {
+  final _repository = Repository();
+  final _userGetter = PublishSubject<User>();
+
+  Observable<User> get getUser => _userGetter.stream;
+
+  registerUser(String firstname, String lastname, String username, String email,
+      String password) async {
+    User user = await _repository.registerUser(
+        firstname, lastname, username, email, password);
+    _userGetter.sink.add(user);
+  }
+
+  signinUser(String username, String password, String apiKey) async {
+    User user = await _repository.signinUser(username, password, apiKey);
+    _userGetter.sink.add(user);
+  }
+
+  dispose() {
+    _userGetter.close();
+  }
+}
+
+final bloc = UserBloc();
